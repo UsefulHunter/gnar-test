a0cd4332ae236894a4dbfcda7527ad5959a7790b
Brian Icaro D. Marques <bi.dm@hotmail.com>
2020-03-08 23:06:57 -0300

    first commit

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+import './Feed.css';
+import MockFeedData from './MockFeedData';
---
+++ src/components/Feed/MockFeedData.js
@@ -0,0 +1,48 @@
+const MockFeedData = [
+  {
+    title: 'How to Find the purrfect pet',
+    link: 'https://www.thepurrfectpet.com/'
+  },
+  {
+    title: 'What to do with your new cat',
+    link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  },
+  {
+    title: 'How to potty-train your cat',
+    link: 'https://www.thepurrfectpet.com/'
+  },
+  {
+    title: 'Raw food diets for cats',
+    link: 'https://www.thepurrfectpet.com/'
+  },
+  {
+    title: 'Homemade treat recipes',
+    link: 'https://www.thepurrfectpet.com/'
+  },
+  {
+    title: 'Our favorite litter brands',
+    link: 'https://www.thepurrfectpet.com/'
+  },
+  {
+    title: 'Are cat breeds a thing?',
+    link: 'https://www.thepurrfectpet.com/'
+  },
+  {
+    title: 'Our favorite caturday activities',
+    link: 'https://www.thepurrfectpet.com/'
+  },
+  {
+    title: 'How to travel with your cat',
+    link: 'https://www.thepurrfectpet.com/'
+  },
+  {
+    title: 'How to manage multiple cats',
+    link: 'https://www.thepurrfectpet.com/'
+  },
+  {
+    title: 'The best cat memes, ranked',
+    link: 'https://www.reddit.com/r/cats/'
+  }
+];
+
+export default MockFeedData;
