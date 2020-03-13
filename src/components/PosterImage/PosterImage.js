a72fad115d7a20d53e6266d474afc0ec1861016c
Brian Icaro D. Marques <brian.icaro.dm@gmail.com>
2020-03-13 11:40:04 -0300

    Finished the form screen, and now going to the view page. After that, tests, details, and that's it.

--- src/components/PosterImage/PosterImage.js
+++ src/components/PosterImage/PosterImage.js
@@ -5,13 +5,11 @@ const PosterImage = () => {
   const url =
     'https://i0.wp.com/gatomainecoon.com.br/wp-content/uploads/2020/03/curiosidades-sobre-gatos-the-lucky-neko-2JcixB1Ky3I-unsplash.jpg?fit=1200%2C675&ssl=1';
   return (
-    <main className="posterImage__main">
-      <h1 className="posterImage__title">
-        Help Make a<br /> Purrrfect Match
-      </h1>
+    <div className="posterImage__main">
+      <h1 className="posterImage__title">Help Make a Purrrfect Match</h1>
       <div className="posterImage_purple-filter"></div>
       <img className="posterImage__image" src={url} alt="some cat" />
-    </main>
+    </div>
   );
 };
 
